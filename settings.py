bf5a92ba63959673bced02833319e5d6322a378e
Daniil <danya.trishchanovskiy.01@mail.ru>
2021-03-31 20:03:09 +0300

    Регулировка скорости корабля

--- settings.py
+++ settings.py
@@ -6,3 +6,6 @@ class Settings():
         self.screen_width = 1200        # Ширина экрана
         self.screen_height = 800        # Высота экрана
         self.bg_color = (230, 230, 230) # Цвет фона
+
+        # Настройки корабля
+        self.ship_speed = 1.5
--- ship.py
+++ ship.py
@@ -5,13 +5,18 @@ class Ship():
     def __init__(self, ai_game):
         """Инициализирует корабль и задает его начальную позицию."""
         self.screen = ai_game.screen
+        self.settings = ai_game.settings
         self.screen_rect = ai_game.screen.get_rect()
 
         # Загружает изображение кораблся и получает прямоугольник.
         self.image = pygame.image.load('images/ship.bmp')
         self.rect = self.image.get_rect()
+
         # Каждый новый корабль появляется у нижнего края экрана
         self.rect.midbottom = self.screen_rect.midbottom
+
+        # Сохранение вещественной координаты центра корабля.
+        self.x = float(self.rect.x)
 
         # Флаг перемещения
         self.moving_right = False
@@ -19,10 +24,14 @@ class Ship():
 
     def update(self):
         """Обновляет позицию корабля с учетом флага."""
+        # Обновляется атрибут x, не rect
         if self.moving_right:
-            self.rect.x += 1
+            self.x += self.settings.ship_speed
         if self.moving_left:
-            self.rect.x -= 1
+            self.x -= self.settings.ship_speed
+
+        # Обновление атрибута rect на основании self.x
+        self.rect.x = self.x
 
     def blitme(self):
         """Рисует корабль в текущей позиции."""
