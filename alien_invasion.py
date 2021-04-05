21c5a40ec822f3925aa0ee39572766b6a6b04731
Daniil <danya.trishchanovskiy.01@mail.ru>
2021-04-05 16:19:49 +0300

    Конец игры. Определение исполняемых частей игры

--- alien_invasion.py
+++ alien_invasion.py
@@ -35,9 +35,12 @@ class AlienInvasion:
         """Запуск основного цикла игры."""
         while True:
             self._check_events()
-            self.ship.update()
-            self.__update_bullets()
-            self._update_aliens()
+
+            if self.stats.game_active:
+                self.ship.update()
+                self.__update_bullets()
+                self._update_aliens()
+
             self._update_screen()
 
     def _check_events(self):
@@ -150,18 +153,21 @@ class AlienInvasion:
     def _ship_hit(self):
         """Обрабатывает столкновение корябля с пришельцем."""
         # Уменьшение ships_left
-        self.stats.ships_left -= 1
+        if self.stats.ships_left > 0:
+            self.stats.ships_left -= 1
 
-        # Очистка списков пришельцев и снарядов.
-        self.aliens.empty()
-        self.bullets.empty()
+            # Очистка списков пришельцев и снарядов.
+            self.aliens.empty()
+            self.bullets.empty()
 
-        # Создание нового флота и размещение корабля в центре.
-        self._create_fleet()
-        self.ship.center_ship()
+            # Создание нового флота и размещение корабля в центре.
+            self._create_fleet()
+            self.ship.center_ship()
 
-        # Пауза.
-        sleep(0.5)
+            # Пауза.
+            sleep(0.5)
+        else:
+            self.stats.game_active = False
 
     def _update_screen(self):
         """Обновляет изображения на экране и отображает новый экран."""
