98dcb9e6f049cca38244a57b01370feaea947e68
Daniil <danya.trishchanovskiy.01@mail.ru>
2021-04-06 16:36:26 +0300

    Вывод уровня

--- alien_invasion.py
+++ alien_invasion.py
@@ -74,6 +74,7 @@ class AlienInvasion:
             self.stats.reset_stats()
             self.stats.game_active = True
             self.sb.prep_score()
+            self.sb.prep_level()
 
             # Очистка списков пришельцев и снарядов.
             self.aliens.empty()
@@ -137,6 +138,10 @@ class AlienInvasion:
             self.bullets.empty()
             self._create_fleet()
             self.settings.increase_speed()
+
+            # Увеличение уровня.
+            self.stats.level += 1
+            self.sb.prep_level()
 
     def _create_fleet(self):
         """Создание флота вторжения."""
